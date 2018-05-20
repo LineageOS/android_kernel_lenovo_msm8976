b5a695d15cf9c94310718faa186f887880429699
Ioana Ciornei <ciorneiioana@gmail.com>
2018-05-21 00:14:33 +0300

    [DNM] memcg: use the google hand-crafted allow_attach function
    Change-Id: I799a9b073fd5a9511e690737ac46db19cc2a1e36
    Signed-off-by: Ioana Ciornei <ciorneiioana@gmail.com>

--- mm/memcontrol.c
+++ mm/memcontrol.c
@@ -6796,10 +6796,22 @@ static int mem_cgroup_can_attach(struct cgroup *cgroup,
 	return ret;
 }
 
-static int mem_cgroup_allow_attach(struct cgroup *cgroup,
-				   struct cgroup_taskset *tset)
+static int mem_cgroup_allow_attach(struct cgroup *cgrp,
+				 struct cgroup_taskset *tset)
 {
-	return subsys_cgroup_allow_attach(cgroup, tset);
+	const struct cred *cred = current_cred(), *tcred;
+	struct task_struct *task;
+
+	cgroup_taskset_for_each(task, cgrp, tset) {
+		tcred = __task_cred(task);
+
+		if ((current != task) && !capable(CAP_SYS_ADMIN) &&
+		    !uid_eq(cred->euid, tcred->uid) &&
+		    !uid_eq(cred->euid, tcred->suid))
+			return -EACCES;
+	}
+
+	return 0;
 }
 
 static void mem_cgroup_cancel_attach(struct cgroup *cgroup,
@@ -6984,24 +6996,6 @@ static void mem_cgroup_move_task(struct cgroup *cont,
 {
 }
 #endif
-
-static int mem_cgroup_allow_attach(struct cgroup *cgrp,
-				 struct cgroup_taskset *tset)
-{
-	const struct cred *cred = current_cred(), *tcred;
-	struct task_struct *task;
-
-	cgroup_taskset_for_each(task, cgrp, tset) {
-		tcred = __task_cred(task);
-
-		if ((current != task) && !capable(CAP_SYS_ADMIN) &&
-		    !uid_eq(cred->euid, tcred->uid) &&
-		    !uid_eq(cred->euid, tcred->suid))
-			return -EACCES;
-	}
-
-	return 0;
-}
 
 /*
  * Cgroup retains root cgroups across [un]mount cycles making it necessary
