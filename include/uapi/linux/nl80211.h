fcff21d46412eec8f48ff13b1ca9f8fee2b9902d
Quallenauge <Hamsi2k@freenet.de>
2020-02-02 23:03:00 +0100

    cfg80211: Fix merge derp which adds duplicate NL80211_ATTR_*_SOCKET_OWNER.
    In 8cecb2fbd6656d8b45f555bd4a70ad1cbfa398a7 the NL80211 attribute
    NL80211_ATTR_SOCKET_OWNER
    was merged. This mixed up the netlink attributes and brought
    up errors on hostapd changes when the a2426829ce426de82d2fa47071ca41ea81c43307
    ( nl80211: Fix NL80211_ATTR_SMPS_MODE encoding )
    was applied. After that commit, the min length check resulted in an error,
    which points to this wrongly applied patch.

--- include/uapi/linux/nl80211.h
+++ include/uapi/linux/nl80211.h
@@ -1546,17 +1546,6 @@ enum nl80211_commands {
  *	owned by the netlink socket that created it and the scheduled scan will
  *	be stopped when the socket is closed.
  *
- * @NL80211_ATTR_SOCKET_OWNER: Flag attribute, if set during interface
- *	creation then the new interface will be owned by the netlink socket
- *	that created it and will be destroyed when the socket is closed.
- *	If set during scheduled scan start then the new scan req will be
- *	owned by the netlink socket that created it and the scheduled scan will
- *	be stopped when the socket is closed.
- *	If set during configuration of regulatory indoor operation then the
- *	regulatory indoor configuration would be owned by the netlink socket
- *	that configured the indoor setting, and the indoor operation would be
- *	cleared when the socket is closed.
- *
  * @NL80211_ATTR_TDLS_INITIATOR: flag attribute indicating the current end is
  *	the TDLS link initiator.
  *
@@ -1926,8 +1915,6 @@ enum nl80211_attrs {
 	NL80211_ATTR_TDLS_PEER_CAPABILITY,
 
 	NL80211_ATTR_IFACE_SOCKET_OWNER,
-
-	NL80211_ATTR_SOCKET_OWNER,
 
 	NL80211_ATTR_CSA_C_OFFSETS_TX,
 	NL80211_ATTR_MAX_CSA_COUNTERS,
